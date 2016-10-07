d3fa03c9343eeb85d8a0b0473738a7ad6a92858c
CaptainDuck <jmdimayuga021@gmail.com>
2016-10-07 16:03:10 +0800

    Wish this works :P

--- src/EnderParkour/Main.php
+++ src/EnderParkour/Main.php
@@ -8,8 +8,11 @@ use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\item\Item;
 use pocketmine\tile\Tile;
+use pocketmine\scheduler\Task;
 use pocketmine\block\Block;
 use pocketmine\tile\Sign;
 use pocketmine\utils\Config;
@@ -25,7 +28,7 @@ class Main extends PluginBase implements Listener{
     public function onEnable(){
         $this->getLogger()->info(C::BLUE. "EnderParkour has been enabled!");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->parkour = new Config($this->getDataFolder(). "parkour.yml", Config::YAML);
+        $this->parkour = new Config($this->getDataFolder(). "worlds/". $player->getLevel()->getName(). "parkour.yml", Config::YAML);
         @mkdir($this->getDataFolder(). "worlds/");
         $this->saveDefaultConfig();
     }
@@ -34,30 +37,33 @@ class Main extends PluginBase implements Listener{
         $this->getServer()->info(C::BLUE. "EnderParkour has been disabled!:o");
         $this->saveDefaultConfig();
     }
-
     public function Checkpoints(PlayerInteractEvent $event){
+        
         $player = $event->getPlayer();
         $block = $event->getBlock();
-        $x = $player->getX();
-        $y = $player->getY();
-        $z = $player->getZ();
-        $pn = $event->getPlayer()->getName();
-        if($block->getId() == 63 or 68){
-            $signature = $player->getLevel()->getTile($block);
-            if(!($signature instanceof Sign)) return;
-            $line = $signature->getText();
-            if($line[0] == $this->getConfig()->get("CheckpointTextSign")){
-                $this->parkour->set($pn,array($x,$y,$z,$player->getLevel()->getName()));
-                $this->parkour->save();
-                $player->sendMessage($this->getConfig()->get("CheckpointMsg"));
-            }
-            if($line[0] == $this->getConfig()->get("FinishTextSign")){
-                $player->sendMessage($this->getConfig()->get("FinishMsg"));
-                $this->getServer()->dispatchCommand($player, $this->getConfig()->get("FinishPlayerCmd"));
-                $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $this->getConfig()->get("FinishConsoleCmd"), str_ireplace("{PLAYER}", $pn));
-                if($this->getConfig()->get("DelParkourDataWhenFinish") == "true"){
-                    $this->parkour->remove($pn);
+        $pklevel = (new Config($this->getDataFolder(). "worlds/". $player->getLevel()->getName(). $player->getLevel()->getName(). ".yml", Config::YAML))->getAll();
+        if($player->getLevel()->getName() == $pklevel["world"]){
+            $x = $player->getX();
+            $y = $player->getY();
+            $z = $player->getZ();
+            $pn = $event->getPlayer()->getName();
+            if($block->getId() == 63 or 68){
+                $signature = $pklevel["world"]->getTile($block);
+                if(!($signature instanceof Sign)) return;
+                $line = $signature->getText();
+                if($line[0] == $this->getConfig()->get("CheckpointTextSign")){
+                    $this->parkour->set($pn,array($x,$y,$z,$player->getLevel()->getName()));
                     $this->parkour->save();
+                    $player->sendMessage($this->getConfig()->get("CheckpointMsg"));
+                }
+                if($line[0] == $this->getConfig()->get("FinishTextSign")){
+                    $player->sendMessage($this->getConfig()->get("FinishMsg"));
+                    $this->getServer()->dispatchCommand($player, $this->getConfig()->get("FinishPlayerCmd"));
+                    $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $this->getConfig()->get("FinishConsoleCmd"), str_ireplace("{PLAYER}", $pn));
+                    if($this->getConfig()->get("DelParkourDataWhenFinish") == "true"){
+                        $this->parkour->remove($pn);
+                        $this->parkour->save();
+                    }
                 }
             }
         }
@@ -65,29 +71,43 @@ class Main extends PluginBase implements Listener{
     
     public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
         switch($cmd->getName()){
-            case "parkour":
+            case "enderparkour":
                 if(!(isset($args[0]))){
-                    $sender->sendMessage(C::ITALIC. "Parkour Commands!\n" .C::ITAlIC. "/parkour create <world name> -> Creates a parkour world!");
-            }
-            if($args[0] == "create" || isset($args[1]) || $sender->hasPermission("enderparkour.cmd.create") || !file_exists($this->getDataFolder(). "worlds/". $args[0]. ".yml")){
-                $this->worlds = new Config($this->getDataFolder(). "worlds/". $args[0]. ".yml", array(
-                    "world" => $args[0]
-                ));
-                $sender->sendMessage(C::ITALIC. C::GREEN. "EnderParkour >> Successfully created a Parkour world named". $args[0]. "!");
+                    if($sender->hasPermission("enderparkour.cmd")){
+                        $sender->sendMessage(C::ITALIC. "EnderParkour Commands!\n" .C::ITAlIC. "/parkour create -> Creates a Parkour world on your world!". C::ITALIC. "/parkour delete <world name> -> Deletes a Parkour world!");
+                    }
+                }
                 
-            }
+                if($args[0] == "create" || $sender->hasPermission("enderparkour.cmd.create") || !file_exists($this->getDataFolder(). "worlds/". $player->getLevel()->getName(). ".yml")){
+                    $this->worlds = new Config($this->getDataFolder(). "worlds/". $player->getLevel()->getName(). $player->getLevel()->getName(). ".yml", Config::YAML, array(
+                        "world" => $player->getLevel()->getName()
+                    ));
+                    $sender->sendMessage(C::ITALIC. C::GREEN. "EnderParkour >> Successfully created a Parkour world named". $player->getLevel()->getName(). "!");
+                    return true;
+                }else{
+                    $sender->sendMessage(C::ITALIC. C::RED. "EnderParkour >> You don't have permission to create a Parkour world or the Parkour world already exists!");
+                }
+                if($args[0] == "delete" || isset($args[1]) || $sender->hasPermission("enderparkour.cmd.delete") || file_exists($this->getDataFolder(). "worlds/". $player->getLevel()->getName(). ".yml")){
+                    unlink(new Config($this->getDataFolder(). "worlds/". $args[0]. $args[0]. ".yml"));
+                    $sender->sendMessage(C::ITALIC. C::GREEN. "EnderParkour >> Successfully deleted the Parkour world named". $args[0]. "!");
+                    return true;
+                }else{
+                    $sender->sendMessage(C::ITALIC. C::RED. "EnderParkour >> Please enter a Parkour world name or You don't have permission to delete a Parkour world or the Parkour world doesn't exist!");
+                    return true;
+                }
         }
     }
     
     public function noVoid(PlayerMoveEvent $event){
         $player = $event->getPlayer();
-        $pn = $player->getName();
-        if($player->getLevel()->getName() == $this->getConfig()->get("NoVoidWorld")){
+        $pklevel = (new Config($this->getDataFolder(). "worlds/". $player->getLevel()->getName(). $player->getLevel()->getName(). ".yml", Config::YAML))->getAll();
+        if($player->getLevel()->getName() == $pklevel["world"]){
+            $pn = $player->getName();
             if($event->getTo()->getFloorY() < 0.5){
                 $cppos = $this->parkour->get($pn);
                 if(is_array($cppos)){
-                        $player->teleport(new Vector3($cppos[0],$cppos[1],$cppos[2],$this->getServer()->getLevelByName($cppos[3])));
-                        $player->sendMessage($this->getConfig()->get("TpedToCheckpointMsg"));
+                    $player->teleport(new Vector3($cppos[0],$cppos[1],$cppos[2],$this->getServer()->getLevelByName($cppos[3])));
+                    $player->sendMessage($this->getConfig()->get("TpedToCheckpointMsg"));
                 }else{
                     $this->getServer()->dispatchCommand($player, $this->getConfig()->get("VoidPlayerCmd"));
                 }
